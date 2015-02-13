0bb2ed8db2cec5ac8f84e5eb1de55ce994881350
danodz <erwan.bourdeau@gmail.com>
2015-02-13 13:06:05 -0500

    added everything

--- files/allMangaPage.js
+++ files/allMangaPage.js
@@ -2,3 +2,20 @@ function submitNew()
 {
     $.ajax("http://localhost:8000/addManga/" + $(".newManga")[0].value);
 }
+
+function toggleIgnore(name, value)
+{
+    $.ajax("http://localhost:8000/toggleIgnore/" + name + "?value=" + value);
+
+
+    if(value == "False")
+    {
+        $("." + name + " a.toggleIgnore").html("N'est pas ignoré");
+        $("." + name + " a.toggleIgnore").attr("onclick", "toggleIgnore('"+ name +"', 'True')");
+    }
+    else
+    {
+        $("." + name + " a.toggleIgnore").html("Ignoré");
+        $("." + name + " a.toggleIgnore").attr("onclick", "toggleIgnore('"+ name +"', 'False')");
+    }
+}
